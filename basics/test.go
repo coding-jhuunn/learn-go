9dd7be3841844d5d98ea1bfcd44b27bbfa8ad013
coding-jhuunn <codingatjhunn@gmail.com>
2024-06-24 10:53:34 +0800

    setup repo for go

---
+++ basics/test.go
@@ -0,0 +1,9 @@
+package main
+
+// if the package is called main, tells go compiler that our code should be compiled in a executable code
+import "fmt"
+
+//if the func name main, it fires automatically
+func main(){
+	fmt.Println(("hello "))
+}
