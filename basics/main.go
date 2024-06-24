6d27e5cc2dbec552ee03cb5dc1377266fd2db037
coding-jhuunn <codingatjhunn@gmail.com>
2024-06-24 16:15:25 +0800

    loop

--- basics/main.go
+++ basics/main.go
@@ -1,26 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	greeting := "hello there friends!"
-	fmt.Println(strings.Contains(greeting,"hello"))
-	//fnc that return boolean if contains the string
-	fmt.Println(strings.ReplaceAll(greeting,"hello","hi"))
-	//fnc that subtitute that target strings, does not replace the origina value
-	fmt.Println(strings.ToUpper(greeting))
-	//fnc upper case
-	fmt.Println(strings.Index(greeting,"ll"))
-	//find the index of targeted string
-	fmt.Println(strings.Split(greeting,""))
+	// x := 0
+	// for x <= 5 {
+	// 	fmt.Println("value of x is:",x)
+	// 	x++
+	// }
 
-	ages :=[]int{45,12,53,8,3,85,35}
-	sort.Ints(ages)
-	fmt.Println(ages)
-	fmt.Println(sort.SearchInts(ages,30))
+	for i := -0; i < 5; i++ {
+		fmt.Println("value of x is:", i)
 
+	}
 }
