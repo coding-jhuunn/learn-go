3527310fa751c65fbae997b268643faca7d9d10d
coding-jhuunn <codingatjhunn@gmail.com>
2024-06-26 13:45:33 +0800

    create object type / struct

--- basics/main.go
+++ basics/main.go
@@ -2,28 +2,7 @@ package main
 
 import "fmt"
 
-func updateName(n *string)  {
-	// now we are passing a pointer
-	//if use * in front of argument that means accpeting in pointer
-	*n = "wedege"
-	
-}
-
 func main() {
-
-	name := "tifa"
-	
-	
-
-	//fmt.Println("memory address of name is: ",&name)
-	//to get the memory address of a variable
-	
-	m:=&name
-	fmt.Println("memory address",m)
-	// use * to get the value of the pointer
-	fmt.Println("value at memory address: ",*m)
-	fmt.Println(name)
-	
-	updateName(m)
-	fmt.Println(name)
+	mybill := newBill("My bill")
+	fmt.Println(mybill)
 }
