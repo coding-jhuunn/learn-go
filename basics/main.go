6be37e7c887662080ba70d8e9403eb566ff16ce0
coding-jhuunn <codingatjhunn@gmail.com>
2024-06-26 16:24:03 +0800

    parsing floats

--- basics/main.go
+++ basics/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -28,18 +29,36 @@ func  createBill() bill{
 
 func promptOptions(b bill){
 	reader:=bufio.NewReader(os.Stdin)
-	opt,_ := getInput("Option: (a - add item , s -save bill, t- add tip",reader)
+	opt,_ := getInput("Option: (a - add item , s -save bill, t- add tip)",reader)
 	switch opt{
 	case "a":
 		name,_:=getInput("Item name",reader)
 		price,_:=getInput("Item Price",reader)
-		fmt.Println(name,price)
-	case "s":
-		tip,_:=getInput("Tip",reader)
 
-		fmt.Println(tip)
+		p,err :=strconv.ParseFloat(price,64)
+		if err !=nil{
+			fmt.Println("The price must be a number")
+			promptOptions(b)
+		}
+
+		b.addItem(name,p)
+		fmt.Println("Item added -",name,p)
+		promptOptions(b)
+	case "s":
+		fmt.Println("you chose to save the bill",b)
 	case "t":
-		fmt.Println("you choose t")
+		tip,_:=getInput("Tip",reader)
+		
+		t,err :=strconv.ParseFloat(tip,64)
+		if err !=nil{
+			fmt.Println("The tip must be a number")
+			promptOptions(b)
+		}
+
+		b.updateTip(t)
+		fmt.Println("Tip added -",t)
+		promptOptions(b)
+		fmt.Println(tip)
 	default:
 		fmt.Println("not valid")
 		promptOptions(b)
