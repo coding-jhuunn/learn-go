57c01317ca21e3cef5bcc141aa477ffb30337555
coding-jhuunn <codingatjhunn@gmail.com>
2024-06-26 10:32:58 +0800

    maps

--- basics/main.go
+++ basics/main.go
@@ -2,15 +2,38 @@ package main
 
 import "fmt"
 
-var score = 99.5
-
 func main() {
 
-	sayHello("HI")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":    4.99,
+		"pie":     7.99,
+		"salad":   6.99,
+		"pudding": 3.55,
 	}
-	showScore(score)
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+
+	//looop
+	//key then value variable
+	for k, v:=range menu{
+		fmt.Println(k,"-",v)
+	}
+
+	// ints as key type
+
+	phonebook :=map[int] string{
+		232123:"Mario",
+		123123:"asd",
+		454454:"asda",
+	}
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[123123])
+
+	//update an item in a map
+
+	phonebook[123123]="bowser"
+	fmt.Println(phonebook[123123])
+
 
 }
